08e9e47e054fff600296abfff5308a9859ac7340
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2026-02-22 15:47:02 +0500

    kernel/sys.c: Fix typo

--- kernel/sys.c
+++ kernel/sys.c
@@ -1245,7 +1245,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	struct new_utsname tmp;
 	uid_t cur_uid = current_uid().val;
 
-        down_read(&uts_sem);
+    down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	if (current_uid().val == 0 &&
 	    (!strncmp(current->comm, "bpfloader", 9) ||
